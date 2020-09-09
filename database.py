9c8d8141cb7a047f380686100e7239afa9023a50
cathimn <catclee@gmail.com>
2020-09-09 04:02:00 -0500

    Model configuration

--- database.py
+++ database.py
@@ -1,25 +1,62 @@
+import os
+from starter_app.models import User, Tag, Post, Support
+from starter_app import app, db
 from dotenv import load_dotenv
 load_dotenv()
 
-from starter_app import app, db
-from starter_app.models import User
+SEED_USERS_PASSWORD = os.environ.get('SEED_USERS_PASSWORD')
 
 with app.app_context():
   db.drop_all()
   db.create_all()
 
-  ian = User(username = 'Ian', email = 'ian@aa.io')
-  javier = User(username = 'Javier', email = 'javier@aa.io')
-  dean = User(username = 'Dean', email = 'dean@aa.io')
-  angela = User(username = 'Angela', email = 'angela@aa.io')
-  soonmi = User(username = 'Soon-Mi', email = 'soonmi@aa.io')
-  alissa = User(username = 'Alissa', email = 'alissa@aa.io')
+  tags = [
+      'Animation', 'Art', 'Comics', 'Drawing & Painting', 'Education',
+      'Food & Drink', 'Gaming', 'Music', 'Other', 'Photography', 'Podcast',
+      'Science & Tech', 'Social', 'Software', 'Streaming', 'Video & Film',
+      'Writing'
+  ]
+  for tag in tags:
+      db.session.add(Tag(tag_name=tag))
 
-  db.session.add(ian)
-  db.session.add(javier)
-  db.session.add(dean)
-  db.session.add(angela)
-  db.session.add(soonmi)
-  db.session.add(alissa)
+  captain = User(username='captaincat',
+                 email='captain@thecat.com',
+                 password = SEED_USERS_PASSWORD)
+  luna = User(username='lunacat',
+              email='luna@thecat.com',
+              password = SEED_USERS_PASSWORD)
+  cath = User(username='cath',
+              email='cath@thecat.com',
+              password = SEED_USERS_PASSWORD)
+
+  db.session.add(captain)
+  db.session.add(luna)
+  db.session.add(cath)
+
+  db.session.add(Post(user_id=1, body="Meow meow!!!"))
+  db.session.add(Post(user_id=1, body="I'm Captain!"))
+  db.session.add(Post(user_id=2, body="Mew mew!!!"))
+  db.session.add(Post(user_id=2, body="I'm Luna~"))
+
+  db.session.add(
+      Post(user_id=3, body="This is Catherine making a test post..."))
+
+  captain.follow(captain)
+  luna.follow(luna)
+  cath.follow(cath)
+  cath.follow(captain)
+  cath.follow(luna)
+
+  test = Support(user_id=2,
+                 supporter_id=3,
+                 amount=1,
+                 body="My favorite cat! <3")
+  test_again = Support(user_id=2,
+                       supporter_id=3,
+                       amount=2,
+                       body="Cool girl. B-)",
+                       private=True)
+  db.session.add(test)
+  db.session.add(test_again)
 
   db.session.commit()
--- starter_app/models.py
+++ starter_app/models.py
@@ -1,17 +1,133 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 db = SQLAlchemy()
+
+following = db.Table(
+    'follows',
+    db.Column('user_id', db.Integer, db.ForeignKey(
+        'users.id'), primary_key=True),
+    db.Column('follow_id', db.Integer, db.ForeignKey(
+        'users.id'), primary_key=True),
+)
+
 
 class User(db.Model):
   __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(40), nullable=False, unique=True)
+  email = db.Column(db.String(255), nullable=False, unique=True)
+  display_name = db.Column(db.String(40))
+  hashed_password = db.Column(db.String(100), nullable=False)
+  session_token = db.Column(db.String(), unique=True)
+  avatar_url = db.Column(db.String())
+  banner_url = db.Column(db.String())
+  accept_payments = db.Column(db.Boolean, default=False)
+  created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+  posts = db.relationship('Post', back_populates='user')
+  follows = db.relationship('User',
+                            secondary=following,
+                            primaryjoin=id == following.c.user_id,
+                            secondaryjoin=id == following.c.follow_id)
+
+  @property
+  def password(self):
+      return self.hashed_password
+
+  @password.setter
+  def password(self, password):
+      self.hashed_password = generate_password_hash(password)
+
+  def check_password(self, password):
+      return check_password_hash(self.password, password)
+
+  def follow(self, to_follow):
+      if to_follow not in self.follows:
+          self.follows.append(to_follow)
+
+  def unfollow(self, to_unfollow):
+      if to_unfollow in self.follows:
+          self.follows.remove(to_unfollow)
+
+  def is_following(self, user_to_check):
+      return user_to_check in self.follows
+
+  def retrieve_feed(self):
+      return [post.to_dict() for post in Post.query.join(
+          following, (following.c.follow_id == Post.user_id)).filter(
+          following.c.user_id == self.id).order_by(Post.created_at.desc())]
+
+
+users_likes = db.Table(
+    'users_likes',
+    db.Column('user_id',
+              db.Integer,
+              db.ForeignKey('users.id'),
+              primary_key=True),
+    db.Column('post_id',
+              db.Integer,
+              db.ForeignKey('posts.id'),
+              primary_key=True))
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    body = db.Column(db.String(140))
+    image_url = db.Column(db.String())
+    created_at = db.Column(db.DateTime,
+                           nullable=False,
+                           default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime)
+
+    user = db.relationship('User', back_populates='posts')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "author": User.query.get(self.user_id).username,
+            "body": self.body,
+            "image_url": self.image_url,
+            "posted_on": str(self.created_at)
+        }
+
+
+class Support(db.Model):
+    __tablename__ = 'supports'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    supporter_id = db.Column(db.Integer,
+                             db.ForeignKey('users.id'),
+                             nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    body = db.Column(db.Text)
+    private = db.Column(db.Boolean)
+
+    supported_user = db.relationship('User', foreign_keys=[user_id])
+    supporting_user = db.relationship('User', foreign_keys=[supporter_id])
+
+    __table_args__ = (db.CheckConstraint(user_id != supporter_id,
+                                         name='cannot_support_yourself'), )
+
+
+users_tags = db.Table(
+    'users_tags',
+    db.Column('user_id',
+              db.Integer,
+              db.ForeignKey('users.id'),
+              primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'),
+              primary_key=True))
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, primary_key=True)
+    tag_name = db.Column(db.String(40), nullable=False, unique=True)
