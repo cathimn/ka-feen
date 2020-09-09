175ea4e8d03f870e5c5580cc7b11cc4a4f9053ad
cathimn <catclee@gmail.com>
2020-09-09 18:01:37 -0500

    Started Flask routes and trying to finalize models

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
-import UserList from './components/UsersList';
+import LandingPage from './components/LandingPage';
+import LoginPage from './components/LoginPage';
+import Newsfeed from './components/Newsfeed';
+
+import { loadToken, logout } from './actions/authentication';
 
 
 function App() {
+    const dispatch = useDispatch();
+    const loggedIn = useSelector(store => store.authentication.token);
 
-  return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            </ul>
-        </nav>
+    useEffect(() => {
+        dispatch(loadToken());
+    }, [])
+
+    return (
+      <BrowserRouter>
+        <NavLink to="/">HOME</NavLink>
+        {loggedIn ? <button onClick={() => dispatch(logout())}>LOG OUT</button> : <NavLink to="/login">LOG IN</NavLink>}
         <Switch>
-            <Route path="/users">
-                <UserList />
-            </Route>
-
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
+          <Route path="/newsfeed">
+            <Newsfeed />
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
         </Switch>
-    </BrowserRouter>
-  );
+      </BrowserRouter>
+    );
 }
 
 export default App;
--- starter_app/api/user_routes.py
+++ starter_app/api/user_routes.py
@@ -1,9 +1,18 @@
 from flask import Blueprint, jsonify
-from starter_app.models import User
+from flask_jwt_extended import jwt_required
+from starter_app.models import db, User, Tag, users_tags
 
 user_routes = Blueprint('users', __name__)
+
+
+@user_routes.route('/search=<query>')
+def search(query):
+  res = User.query.filter(db.or_(User.username.like(
+      f'%{query}%'), User.display_name.like(f'%{query}%')))
+  return {"results": [result.to_dict() for result in res]}
+
 
 @user_routes.route('/')
 def index():
   response = User.query.all()
-  return { "users": [user.to_dict() for user in response]}
+  return {"users": [user.id for user in response]}
