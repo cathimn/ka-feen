5d413152ce4ad6eb766d1bf688f9123213a558a0
cathimn <catclee@gmail.com>
2020-10-07 23:58:35 -0500

    Updated landing

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -17,7 +17,7 @@ export default function () {
     <>
       <Navbar />
       <div className="container">
-        <img src="https://kafeen.s3.us-east-2.amazonaws.com/IMG_0852.PNG" width="1000px" alt="cartoon caffeinated drinks"/>
+        <img src="https://kafeen.s3.us-east-2.amazonaws.com/IMG_0852.PNG" width="1000px" height="min-content" alt="cartoon caffeinated drinks"/>
         <div style={{position: "absolute", top: "175px", textAlign: "center"}}>
           <h1 style={{ fontSize: "50px"}}>Fund Your Passions</h1>
           <p style={{ width: "450px" }}>The friendly and free way for fans to support your work for the price of your favorite caffeinated beverage. *</p>
